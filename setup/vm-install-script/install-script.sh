6a36dca68f45ed0a731696c16f24fa6d2e33466c
root <root@devsecops-cloud.x23yilqujctellr40lcvck10qe.rx.internal.cloudapp.net>
2022-10-05 14:04:01 +0000

    updated the install script

--- setup/vm-install-script/install-script.sh
+++ setup/vm-install-script/install-script.sh
@@ -50,7 +50,8 @@ kubeadm init --kubernetes-version=${KUBE_VERSION} --skip-token-print
 mkdir -p ~/.kube
 sudo cp -i /etc/kubernetes/admin.conf ~/.kube/config
 
-kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+#kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+kubectl apply -f https://github.com/coreos/flannel/raw/master/Documentation/kube-flannel.yml
 
 sleep 60
 
