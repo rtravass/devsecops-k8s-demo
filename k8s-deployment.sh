8d1bcab10176d471cb1a2af5ff3fe8ee5aab3337
rtravass <ryantravasso@gmail.com>
2022-10-08 22:42:09 +0530

    updated Jenkins to add Kubesc scan for k8s

--- k8s-deployment.sh
+++ k8s-deployment.sh
@@ -3,13 +3,19 @@
 #k8s-deployment.sh
 
 sed -i "s#replace#${imageName}#g" k8s_deployment_service.yaml
-kubectl -n default get deployment ${deploymentName} > /dev/null
 
-if [[ $? -ne 0 ]]; then
-    echo "deployment ${deploymentName} doesnt exist"
-    kubectl -n default apply -f k8s_deployment_service.yaml
-else
-    echo "deployment ${deploymentName} exist"
-    echo "image name - ${imageName}"
-    kubectl -n default set image deploy ${deploymentName} ${containerName}=${imageName} --record=true
-fi
+# commented because the below logic doesnt apply the new k8s manifest for existing deployments.
+# instead only updates the container image.
+
+# kubectl -n default get deployment ${deploymentName} > /dev/null
+
+# if [[ $? -ne 0 ]]; then
+#     echo "deployment ${deploymentName} doesnt exist"
+#     kubectl -n default apply -f k8s_deployment_service.yaml
+# else
+#     echo "deployment ${deploymentName} exist"
+#     echo "image name - ${imageName}"
+#     kubectl -n default set image deploy ${deploymentName} ${containerName}=${imageName} --record=true
+# fi
+
+kubectl -n default apply -f k8s_deployment_service.yaml
